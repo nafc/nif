76c85362cc43c3416a085bed443485e1d5cbc839
FuzzyLitchi <blemille@hotmail.com>
2017-04-30 00:56:57 +0200

    update file/data so that it actually fits the specs

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ mod tests {
     #[test]
     fn from_bytes() {
         let bytes: Vec<u8> = vec![
-            0x6e, 0x69, 0x66, 0x01, //file identifier
+            0x6e, 0x69, 0x66, //file identifier
             0x01, //2px width
             0x02, //3px height
             0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //6 pixels indexing first color in palette
