2178185e10bb757d33e41153be3495b377138eac
Emile Yvan Boutet Livoff <blemille@hotmail.com>
2017-04-29 18:21:48 +0200

    Add Palette to image.rs

--- src/nif/image.rs
+++ src/nif/image.rs
@@ -1,25 +1,30 @@
 use nif::Pixel;
+use nif::Palette;
+use nif::Color;
 
 pub struct Image {
-    width:  usize,
-    height: usize,
-    pixels: Vec<Pixel>,
+    width:   usize,
+    height:  usize,
+    pixels:  Vec<Pixel>,
+    palette: Palette,
 }
 
 impl Image {
-    pub fn new(width: usize, height: usize, pixels: Vec<Pixel>) -> Image {
+    pub fn new(width: usize, height: usize, pixels: Vec<Pixel>, palette: Palette) -> Image {
         Image {
-            width:  width,
-            height: height,
-            pixels: pixels,
+            palette: palette,
+            width:   width,
+            height:  height,
+            pixels:  pixels,
         }
     }
 
     pub fn empty(width: usize, height: usize) -> Image {
         Image {
-            width:  width,
-            height: height,
-            pixels: vec![Pixel::new(0); width * height],
+            palette: Palette::new(vec![Color::RGB(0,0,0)]),
+            width:   width,
+            height:  height,
+            pixels:  vec![Pixel::new(0); width * height],
         }
     }
 }
